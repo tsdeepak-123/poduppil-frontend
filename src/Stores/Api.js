1303332d15d261b1c29d3d24c7976204a9aca40d
Deepak ts <tsdeepak589@gmail.com>
2023-12-10 12:53:32 +0530

    link added

--- src/Stores/Api.js
+++ src/Stores/Api.js
@@ -1,3 +1,3 @@
-export const userApi ='http://localhost:5001/'
+export const userApi ='https://poduppilconstructions.onrender.com/'
 
-export const adminApi ='http://localhost:5001/admin/'
+export const adminApi ='https://poduppilconstructions.onrender.com/admin/'
