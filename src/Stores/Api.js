1422b41928c1c1a5c1810d906b97b44eabba1afe
Deepak ts <tsdeepak589@gmail.com>
2024-03-23 17:45:29 +0530

    url test

--- src/Stores/Api.js
+++ src/Stores/Api.js
@@ -2,11 +2,16 @@
 
 
 // export const adminApi ='https://poduppilconstructions.onrender.com/admin/'
-export const userApi ='https://poduppil-backend.onrender.com/'
+// export const userApi ='https://poduppil-backend.onrender.com/'
 
 
-export const adminApi ='https://poduppil-backend.onrender.com/admin/'
+// export const adminApi ='http://poduppil-backend.onrender.com/admin/'
 // export const userApi ='http://localhost:5001/'
 
 
 // export const adminApi ='http://localhost:5001/admin/'    
+
+export const userApi ="https://backend.poduppilconstructions.com/"
+
+
+export const adminApi ="https://backend.poduppilconstructions.com/admin/" 
