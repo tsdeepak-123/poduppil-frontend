ec1ad954e545fe455f00c85bae0586e8dde44234
Deepak ts <tsdeepak589@gmail.com>
2024-03-25 15:01:58 +0530

    loading added in materials

--- src/Stores/Api.js
+++ src/Stores/Api.js
@@ -1,11 +1,11 @@
 
 
-export const userApi ='http://localhost:5003/'
+// export const userApi ='http://localhost:5003/'
 
 
-export const adminApi ='http://localhost:5003/admin/'    
+// export const adminApi ='http://localhost:5003/admin/'    
 
-// export const userApi ='https://server.poduppilconstructions.com'
+export const userApi ='https://server.poduppilconstructions.com'
 
 
-// export const adminApi ='https://server.poduppilconstructions.com/admin'
+export const adminApi ='https://server.poduppilconstructions.com/admin'
